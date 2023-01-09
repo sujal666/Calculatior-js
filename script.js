761245ef712a3d6a02d7d72f7f65dcc5da331f1f
Sujal Poojari <84837114+sujal666@users.noreply.github.com>
2023-01-09 19:45:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let string = "";
+let buttoms = document.querySelectorAll('.button');
+Array.from(buttoms).forEach((button)=>{
+     button.addEventListener('click',(e)=>{
+       if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value=string;
+       }
+      else if(e.target.innerHTML == 'C'){
+        string ="";
+        document.querySelector('input').value=string;
+       }
+       else{
+       console.log(e.target)
+       string = string + e.target.innerHTML;
+       document.querySelector('input').value=string;
+       }
+     })
+})
